1cd71fb15916134b4cbe79c0b1b6bccd99882bcd
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-07 05:42:07 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  all_true = true
+  while i < collection.size and all_true
+    all_true and yield(collection[i]) ? all_true = true : all_true = false
+    i += 1
+  end
+  all_true
 end
