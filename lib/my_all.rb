e5703219508e2e36373b4b636ca56df5b92e54da
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-07 05:39:57 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   all_true = true
   while i < collection.size and all_true
-    all_true = all_true and yield collection[i]
+    all_true = all_true and yield collection[i] ? true : false
     i += 1
   end
   all_true
