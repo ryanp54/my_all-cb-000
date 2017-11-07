36fd2467dddac183ff584a0b0c33ed3c76a89e44
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-07 05:38:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  all_true = true
+  while i < collection.size and all_true
+    all_true = all_true and yield collection[i]
+  end
+  all_true
 end
